778d3ad1bdf9cb5ec8ed28748401d5cf9d47765a
devX-shub <161881953+devX-shub@users.noreply.github.com>
2024-03-10 02:32:32 +0530

    NumbersJavaScriptOperations

---
+++ NumberAndMaths/index.js
@@ -0,0 +1,7 @@
+const score = 200;
+console.log(typeof score);
+const balance = new Number(100)
+console.log(balance);
+console.log(typeof score.toString());
+console.log(score.toString().length);
+
